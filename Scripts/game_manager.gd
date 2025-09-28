aeddf56c684565a1cbaadbd27f49df5879e9cc5a
Affan Khan <khanaffan1006@gmail.com>
2025-09-28 12:22:26 +1000

    added fruits

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -2,10 +2,16 @@ extends Node
 
 var score = 0
 
+var fruits = 0
 
 @onready var score_label: Label = $ScoreLabel
+@onready var fruits_label: Label = $FruitsLabel
 
 
 func add_score():
 	score += 1
 	score_label.text = "You collected " + str(score) + " coins!"
+
+func add_fruits():
+	fruits += 1
+	fruits_label.text = "You have " + str(fruits) + " fruits for survival..."
